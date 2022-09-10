71e6841d6a43d32bfbe2669cceef6155fc50ad17
Peterskill <104547222+Peterskill@users.noreply.github.com>
2022-09-10 17:14:37 +0530

    Update SNC.h

--- SNC.h
+++ SNC.h
@@ -1,14 +1,21 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
+#include <time.h>
+#include <sys/time.h>
 #include <string.h>
-#include <pthread.h>
+#include <errno.h>
+#include <netdb.h>
+#include <sys/uio.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <sys/types.h>
-#include <signal.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <syslog.h>
 
 #define MAX_CLIENT 10
 #define MAX_IDLE_WAIT 300
