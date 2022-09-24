181303290a70176dbfc6d687d35bbfeb2339855f
Peterskill <104547222+Peterskill@users.noreply.github.com>
2022-09-24 11:42:08 +0530

    Initial Setup

--- SNCServer.c
+++ SNCServer.c
@@ -1,40 +1,80 @@
-#include <SNC.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-int main(int argc, char **argv)
-{
-	int			listenfd, connfd;
-	pid_t			childpid;
-	socklen_t		clilen;
-	struct sockaddr_in	cliaddr, servaddr;
+#define PORT 4444
 
-	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+int main(){
 
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family      = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port        = htons(SERV_TCP_PORT);
+	int sockfd, ret;
+	 struct sockaddr_in serverAddr;
 
-	// Activate the following code to so that the parent server can be re-run even when the child server is still running.
-      
-	if(setsockopt (listenfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-                perror("setsockopt failed");
-                exit (1);
-        }
-	
+	int newSocket;
+	struct sockaddr_in newAddr;
 
-	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+	socklen_t addr_size;
 
-	Listen(listenfd, LISTENQ);
+	char buffer[1024];
+	pid_t childpid;
 
-	for ( ; ; ) {
-		clilen = sizeof(cliaddr);
-		connfd = Accept(listenfd, (SA *) &cliaddr, &clilen);
-
-		if ( (childpid = Fork()) == 0) {	/* child process */
-			Close(listenfd);	/* close listening socket */
-			str_echo(connfd);	/* process the request */
-			exit(0);
-		}
-		Close(connfd);			/* parent closes connected socket */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0){
+		printf("[-]Error in connection.\n");
+		exit(1);
 	}
+	printf("[+]Server Socket is created.\n");
+
+	memset(&serverAddr, '\0', sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	ret = bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	if(ret < 0){
+		printf("[-]Error in binding.\n");
+		exit(1);
+	}
+	printf("[+]Bind to port %d\n", 4444);
+
+	if(listen(sockfd, 10) == 0){
+		printf("[+]Listening....\n");
+	}else{
+		printf("[-]Error in binding.\n");
+	}
+
+
+	while(1){
+		newSocket = accept(sockfd, (struct sockaddr*)&newAddr, &addr_size);
+		if(newSocket < 0){
+			exit(1);
+		}
+		printf("Connection accepted from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
+
+		if((childpid = fork()) == 0){
+			close(sockfd);
+
+			while(1){
+				recv(newSocket, buffer, 1024, 0);
+				if(strcmp(buffer, ":exit") == 0){
+					printf("Disconnected from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
+					break;
+				}else{
+					printf("Client: %s\n", buffer);
+					send(newSocket, buffer, strlen(buffer), 0);
+					bzero(buffer, sizeof(buffer));
+				}
+			}
+		}
+
+	}
+
+	close(newSocket);
+
+
+	return 0;
 }
